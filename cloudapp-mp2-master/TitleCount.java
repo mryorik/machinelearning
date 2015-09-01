f33e8ad3c25d66c61858412e826a00b8fb727bb4
Jaroslav Frolikov <mr.yorik@gmail.com>
2015-09-01 23:22:18 +0300

    MP2 TitlesCount

---
+++ cloudapp-mp2-master/TitleCount.java
@@ -0,0 +1,116 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// >>> Don't Change
+public class TitleCount extends Configured implements Tool {
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new TitleCount(), args);
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = Job.getInstance(this.getConf(), "Title Count");
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setMapperClass(TitleCountMap.class);
+        job.setReducerClass(TitleCountReduce.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setJarByClass(TitleCount.class);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static String readHDFSFile(String path, Configuration conf) throws IOException{
+        Path pt=new Path(path);
+        FileSystem fs = FileSystem.get(pt.toUri(), conf);
+        FSDataInputStream file = fs.open(pt);
+        BufferedReader buffIn=new BufferedReader(new InputStreamReader(file));
+
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while( (line = buffIn.readLine()) != null) {
+            everything.append(line);
+            everything.append("\n");
+        }
+        return everything.toString();
+    }
+
+// <<< Don't Change
+
+    public static class TitleCountMap extends Mapper<Object, Text, Text, IntWritable> {
+        List<String> stopWords;
+        String delimiters;
+
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+
+            Configuration conf = context.getConfiguration();
+
+            String stopWordsPath = conf.get("stopwords");
+            String delimitersPath = conf.get("delimiters");
+
+            this.stopWords = Arrays.asList(readHDFSFile(stopWordsPath, conf).split("\n"));
+            this.delimiters = readHDFSFile(delimitersPath, conf);
+        }
+
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer st = new StringTokenizer(value.toString(), delimiters);
+
+            Map<String, Integer> words = new HashMap<String, Integer>();
+
+            while (st.hasMoreTokens()) {
+                String token = st.nextToken();
+                if (stopWords.contains(token)) {
+                    continue;
+                }
+
+                Integer c = words.get(token) == null ? 0 : words.get(token);
+                words.put(token, ++c);
+            }
+
+            for (Map.Entry<String, Integer> e: words.entrySet()) {
+                context.write(new Text(e.getKey()), new IntWritable(e.getValue()));
+            }
+        }
+    }
+
+    public static class TitleCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            IntWritable total = new IntWritable(0);
+            for (IntWritable v : values) {
+                total.set(total.get() + v.get());
+            }
+
+            context.write(key, total);
+        }
+    }
+}
