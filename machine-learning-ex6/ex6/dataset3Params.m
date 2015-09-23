d40040748470774c7335c26d3c852430bde043bb
Jaroslav Frolikov <mr.yorik@gmail.com>
2015-09-24 01:12:15 +0300

    сделал упражнения недели 7

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,6 +23,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+predictions = svmPredict(svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)), Xval);
+min_err = mean(double(predictions ~= yval));
+
+for C_test = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+    for sigma_test = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+        predictions = svmPredict(svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test)), Xval);
+        err = mean(double(predictions ~= yval));
+        if (err < min_err)
+            min_err = err;
+            C = C_test;
+            sigma = sigma_test;
+        end
+    end
+end
 
 
 
